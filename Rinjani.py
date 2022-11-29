9e71cd6bc97b345850ae0bbc4118330fe2fe2818
erwinsugianto06 <118832986+erwinsugianto06@users.noreply.github.com>
2022-11-30 00:51:06 +0700

    Update Rinjani.py

--- Rinjani.py
+++ Rinjani.py
@@ -116,7 +116,7 @@ if Jumlah ==3: potongan = (Jumlah*Harga)*7/100
 elif Jumlah >3: potongan = (Jumlah*Harga)*10/100
 else : potongan =0
 
-Total = (Jumlah*Harga*Lama)-str(potongan)
+Total = (Jumlah*Harga*Lama)-int(potongan)
 Pajak = int(Total*0.11)
 Jumlah_bayar = int(Total+Pajak)
 def garis():
