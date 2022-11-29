1695de5c98581ab603eb1d4b6ba5fbb5ab945dd8
erwinsugianto06 <118832986+erwinsugianto06@users.noreply.github.com>
2022-11-30 01:27:05 +0700

    Update Rinjani.py

--- Rinjani.py
+++ Rinjani.py
@@ -112,10 +112,10 @@ while Kode :
 Note =st.text_area('Catatan')
 
 st.write()
-a = if Jumlah ==3: potongan = (Jumlah*Harga)*7/100 elif Jumlah >3: potongan = (Jumlah*Harga)*10/100 else : potongan =0
+if Jumlah ==3: potongan = (Jumlah*Harga)*7/100 elif Jumlah >3: potongan = (Jumlah*Harga)*10/100 else : potongan =0
 
 
-Total = (Jumlah*Harga*Lama)-a
+Total = (Jumlah*Harga*Lama)-Potongan
 Pajak = int(Total*0.11)
 Jumlah_bayar = int(Total+Pajak)
 def garis():
